ebae32a1c8fb13f54c6d457c53fcba65a386ef8d
John Derr <john.derr@maximos.io>
2020-06-03 15:38:22 -0400

    text and styling

--- src/App.js
+++ src/App.js
@@ -6,19 +6,19 @@ import Covfefe from './Covfefe';
 
 const App = () => {
   return(
-    <div>
+    <div className="p-4">
       <Covfefe />
-        <p>
-          Credit to <a href='https://codegolf.stackexchange.com/questions/123685/covfefify-a-string'>this thread</a> from the <a href='codegolf.stackoverflow.com'>codegolf</a> section of stackoverflow for the regex to covfefiy a string.
-        </p>
+      <p>
+        Credit to <a  className='text-blue-800 bold' href='https://codegolf.stackexchange.com/questions/123685/covfefify-a-string'>this thread</a> from the <a class='text-blue-800 bold' href='https://codegolf.stackoverflow.com'>codegolf</a> section of stackoverflow for the regex to covfefiy a string.
+      </p>
+      <div className='mt-4'>
+        <b>Built with:</b>
         <ul>
           <li>React</li>
           <li>Tailwind CSS</li>
           <li>Parcel</li>
         </ul>
-        <p>
-          Open Source is awesome, even if our president sucks.
-        </p>
+      </div>
     </div>
   );
 };
