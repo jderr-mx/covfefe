cf4be579041899741c0c1807389502a3d50d373d
John Derr <john.derr@maximos.io>
2020-06-03 12:12:37 -0400

    refactored into functional component with hooks, TODO make it pretty

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import { render } from 'react-dom';
+import UncleJoe from './uncle_joe.jpg';
 import './app.css';
 
-// import Covfefe from './Covfefe';
+import Covfefe from './Covfefe';
 
 const App = () => {
-  return <h1>Hello World</h1>;
+  return(
+    <Fragment>
+      <Covfefe />
+      <img src={UncleJoe}/>
+        <p>
+          Credit to <a href='https://codegolf.stackexchange.com/questions/123685/covfefify-a-string'>this thread</a> from the <a href='codegolf.stackoverflow.com'>codegolf</a> section of stackoverflow for the regex to covfefiy a string.
+        </p>
+        <ul>
+          <li>React</li>
+          <li>Tailwind CSS</li>
+          <li>Parcel</li>
+        </ul>
+        <p>
+          Open Source is awesome, even if our president sucks.
+        </p>
+    </Fragment>
+  );
 };
-//        <Covfefe/>
-//        <p>
-//          Credit to <a href='https://codegolf.stackexchange.com/questions/123685/covfefify-a-string' target='_blank'>this thread</a> from the <a href='codegolf.stackoverflow.com'>codegolf</a> section of stackoverflow for the regex to covfefiy a string.
-//        </p>
-//        <p>
-//          Built with <a href='https://github.com/facebookincubator/create-react-app'>create-react-app</a> and <a href='https://react.semantic-ui.com'>semantic-ui-react</a>
-//        </p>
-//        <p>
-//          Open Source is awesome, even if our president is not.
-//        </p>
 
 render(<App />, document.getElementById('root'));
--- src/Covfefe.js
+++ src/Covfefe.js
@@ -1,68 +1,42 @@
-import React, { Component, Fragment } from 'react';
-// import { ShareButtons } from 'react-share';
-// import UncleJoe from './uncle_joe.jpg';
+import React, { Component, Fragment, useState } from 'react';
 import _ from 'lodash';
 
 
-class Covfefe extends Component {
+const Covfefe = () => {
+  const [myText, updateMyText] = useState('Coverage');
 
-  constructor(props) {
-    super(props);
-    this.state = { someText: 'Coverage'}
-    this.updateCovfefe = this.updateCovfefe.bind(this);
-  }
+  return (
+    <Fragment>
+      <p>Your Text</p>
+      <textarea onChange={e => updateMyText(e.target.value)} defaultValue={myText} />
+      <p>Your Text on Covfefe</p>
+      <textarea readOnly={true} value={covfefeThis(myText)} />
+    </Fragment>
+  );
+};
 
-  updateCovfefe(e) {
-    e.preventDefault();
-    this.setState({ someText: e.target.value});
-  }
-  render() {
-    return(
-      <Fragment>
-        //<Image src={UncleJoe} />
-        <p>Your Text</p>
-        <textarea onChange={this.updateCovfefe} defaultValue={this.state.someText} />
-        <p>Your Text on Covfefe</p>
-        <textarea readOnly="true" value={covfefeThis(this.state.someText)} />
-          /**
-          <TwitterShareButton url="localhost" title={covfefeThis(this.state.someText)} hashtags={['CovfefeThis!']}>
-            <Label as='a' color='blue'>
-              <Icon name='twitter' />Tweet
-            </Label>
-          </TwitterShareButton>
-          */
-      </Fragment>
-    )
-  }
-}
-
-const f = (s) => {
+const f = s => {
   let results = s.match`(.*?[aeiouy]+(.)).*?([aeiouy])`;
-  if(results === null) {
+  if (results === null) {
     return '';
   }
-  let [,a,b,c] = results;
+  let [, a, b, c] = results;
 
-  return a+(b=(a="bcdfgszkvtgp")[11-a.search(b)]||b)+c+b+c;
-}
+  return a + (b = (a = 'bcdfgszkvtgp')[11 - a.search(b)] || b) + c + b + c;
+};
 
-
-const covfefeThis = (text) => {
-
+const covfefeThis = text => {
   let results = text.match(/\S+\s*/g);
-  return _
-    .chain(results)
-    .map((value) => {
+  return _.chain(results)
+    .map(value => {
       return f(value) || value;
     })
     .reduce((value, v) => {
-      if(!value)
-        return `${v.trim()}`;
-      else
-        return `${value} ${v.trim()}`;
+      if (!value) return `${v.trim()}`;
+      else return `${value} ${v.trim()}`;
     }, '')
     .value();
-}
+};
 
 // const { TwitterShareButton } = ShareButtons;
 
